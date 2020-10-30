84310c596c3d1bb2b0522a66b260dfe5e16776c4
Milan Shrestha <milanshrestha17@hotmail.com>
2020-10-30 12:43:00 -0400

    Modified Employer and Skill model classes to inherit from AbstractEntity class

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description cannot be blank")
+    @Size(min = 3, max = 500, message = "Description should be between 3 to 500 characters")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
