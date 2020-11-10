5e27b5af30bfdf15c62b0454a136c66435bfe76f
Milan Shrestha <milanshrestha17@hotmail.com>
2020-11-10 10:12:45 -0500

    Implemented Many-to-Many relationship between Job and Skill model classes

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Skill extends AbstractEntity {
     @NotBlank(message = "Description cannot be blank")
     @Size(min = 3, max = 500, message = "Description should be between 3 to 500 characters")
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -23,5 +29,9 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
